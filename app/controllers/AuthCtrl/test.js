73ae15f0f6455e24113d0391b1425cd97379e18f
Remah Amr <remah.alarabi@hamza.solutions>
2020-12-06 14:28:38 +0200

    add files

--- app/controllers/AuthCtrl/test.js
+++ app/controllers/AuthCtrl/test.js
@@ -2,10 +2,10 @@ const $baseCtrl = require('../$baseCtrl');
 const { APIResponse } = require('../../utils');
 const models = require('../../models')
 module.exports = $baseCtrl(async (req, res) => {
-  let user = await models._user.findOne({phone: '+201212754599'})
+  let user = await models._user.findOne({phone: req.body.phone})
 //   let user = await models._user.find()
  if(user){
     await user.delete()
  }
-   return APIResponse.Ok(res,'A7la msa beka')     
+   return APIResponse.Ok(res,'A7la msa')     
 });
