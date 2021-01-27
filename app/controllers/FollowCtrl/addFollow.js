6aae677566bf1ef61f2f80f6a75a4fe70e7e071b
Remah Amr <remah.alarabi@hamza.solutions>
2021-01-27 21:26:08 +0200

    handle minor things

--- app/controllers/FollowCtrl/addFollow.js
+++ app/controllers/FollowCtrl/addFollow.js
@@ -1,12 +1,20 @@
 const $baseCtrl = require("../$baseCtrl");
 const models = require("../../models");
 const { APIResponse } = require("../../utils");
+const moment = require("moment");
 
 module.exports = $baseCtrl(async (req, res) => {
   const id = parseInt(req.params.id);
   if (isNaN(id)) return APIResponse.BadRequest(res);
   const user = await models._user.findById(id);
   if (!user) return APIResponse.NotFound(res, "No User with that id");
+  let prevFollow = await models.follow.findOne({
+    receiver: id,
+    createdAt: {
+      $gte: moment().utc().subtract(5, "hour").toDate(),
+    },
+  });
+  if (prevFollow) return APIResponse.Ok(res, prevFollow);
   let follow = await new models.follow({
     sender: req.me.id,
     receiver: id,
