b389d7ce2a0d019baec2e00744aebc1d4ed1d845
Remah Amr <remah.alarabi@hamza.solutions>
2021-05-16 09:45:53 +0200

    sort descending of cities

--- app/controllers/WeatherCtrl/fetchAll.js
+++ app/controllers/WeatherCtrl/fetchAll.js
@@ -2,6 +2,10 @@ const $baseCtrl = require("../$baseCtrl");
 const models = require("../../models");
 const { APIResponse } = require("../../utils");
 module.exports = $baseCtrl(async (req, res) => {
-  let weather = await models.weather.aggregate([]);
+  let weather = await models.weather.aggregate([
+    {
+      $sort: { createdAt: -1 },
+    },
+  ]);
   return APIResponse.Ok(res, weather);
 });
