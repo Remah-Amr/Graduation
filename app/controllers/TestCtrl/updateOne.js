7f9402eb61491699e4a8f828f8afcbeefd51fef4
Remah Amr <remah.alarabi@hamza.solutions>
2021-06-28 21:18:43 +0200

    test

--- app/controllers/TestCtrl/updateOne.js
+++ app/controllers/TestCtrl/updateOne.js
@@ -1,15 +1,25 @@
 const $baseCtrl = require("../$baseCtrl");
 const models = require("../../models");
 const { APIResponse } = require("../../utils");
+const cloudinaryStorage = require("../../services/cloudinaryStorage");
 
-module.exports = $baseCtrl(async (req, res) => {
-  const id = parseInt(req.params.id);
-  if (isNaN(id)) return APIResponse.NotFound(res);
+module.exports = $baseCtrl(
+  [{ name: "photo", maxCount: 1 }],
+  cloudinaryStorage,
+  async (req, res) => {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) return APIResponse.NotFound(res);
 
-  const test = await models.test.findById(id);
-  if (!test) return APIResponse.NotFound(res, "No test with that id");
+    const test = await models.test.findById(id);
+    if (!test) return APIResponse.NotFound(res, "No test with that id");
 
-  await test.set(req.body).save();
+    // Upload photo if enter by user
+    if (req.files && req.files["photo"]) {
+      req.body.photo = req.files["photo"][0].secure_url;
+    }
 
-  return APIResponse.Ok(res, test);
-});
+    await test.set(req.body).save();
+
+    return APIResponse.Ok(res, test);
+  }
+);
