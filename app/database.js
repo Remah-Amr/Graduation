40e3f01ad2bdec9148a92698109df6c4212e7300
Remah Amr <remah.alarabi@hamza.solutions>
2021-06-28 21:32:08 +0200

    test

--- app/database.js
+++ app/database.js
@@ -5,7 +5,7 @@ module.exports = {
   connect: (cb) => {
     return mongoose
       .connect(
-        "mongodb+srv://mohamed:XPhMw6APxmqcY48d@cluster0.1wylb.mongodb.net/any?retryWrites=true&w=majority",
+        "mongodb+srv://Admin:Admin123456@indzone.qv991.mongodb.net/hellohello?retryWrites=true&w=majority",
         {
           useNewUrlParser: true,
           useCreateIndex: true,
