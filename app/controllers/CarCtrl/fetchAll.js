9ac78c3ed2baac1c59c98b3de99528a5fe09ca01
Remah Amr <remah.alarabi@hamza.solutions>
2021-07-12 17:51:43 +0200

    test

--- app/controllers/CarCtrl/fetchAll.js
+++ app/controllers/CarCtrl/fetchAll.js
@@ -3,17 +3,14 @@ const models = require("../../models");
 const { APIResponse } = require("../../utils");
 const bcrypt = require("bcryptjs");
 
-
-module.exports = $baseCtrl(
-    async (req, res) => {
-        let cars = await models._car.fetchAll(
-            req.allowPagination,
-            {},
-            {
-                ...req.queryOptions,
-                populate: ['owner', 'cars', 'gove1', 'gove2']
-            }
-        )
-        return APIResponse.Ok(res, cars)
+module.exports = $baseCtrl(async (req, res) => {
+  let cars = await models._car.fetchAll(
+    req.allowPagination,
+    { ...req.queryFilter },
+    {
+      ...req.queryOptions,
+      populate: ["owner", "cars", "gove1", "gove2"],
     }
-);
+  );
+  return APIResponse.Ok(res, cars);
+});
