ed57ddd9cff9566d8d2b5fb2af21c775f9ad5ade
Remah Amr <remah.alarabi@hamza.solutions>
2021-07-16 18:27:46 +0200

    fix bug

--- app/controllers/JourneyCtrl/endJourney.js
+++ app/controllers/JourneyCtrl/endJourney.js
@@ -2,25 +2,26 @@ const $baseCtrl = require("../$baseCtrl");
 const models = require("../../models");
 const { APIResponse } = require("../../utils");
 
-module.exports = $baseCtrl(
-    async (req, res) => {
-        // end journey 
-        const car = await models._car.findById(req.me.current_car)
-        if (!car.current_journey) return APIResponse.Forbidden(res, 'NO Journey started for this car')
-        if (car.current_driver !== req.me.id) return APIResponse.Forbidden(res, 'Dont allow to end this journey')
+module.exports = $baseCtrl(async (req, res) => {
+  // end journey
+  const car = await models._car.findById(req.me.current_car);
+  if (!car) return APIResponse.NotFound(res, "No Car TO ENd journey on it");
+  if (!car.current_journey)
+    return APIResponse.Forbidden(res, "NO Journey started for this car");
+  if (car.current_driver !== req.me.id)
+    return APIResponse.Forbidden(res, "Dont allow to end this journey");
 
-        let journey = await models.journey.findById(car.current_journey)
-        await journey.set({ status: 'end' }).save()
+  let journey = await models.journey.findById(car.current_journey);
+  await journey.set({ status: "end" }).save();
 
-        car.current_journey = undefined
-        car.current_driver = undefined
+  car.current_journey = undefined;
+  car.current_driver = undefined;
 
-        await car.save()
+  await car.save();
 
-        req.me.current_journey = undefined
-        req.me.current_car = undefined
-        await req.me.save()
+  req.me.current_journey = undefined;
+  req.me.current_car = undefined;
+  await req.me.save();
 
-        return APIResponse.Created(res, journey)
-    }
-);
+  return APIResponse.Created(res, journey);
+});
