5b14537fa28d66f0c7bdbe9dc9dda2f377d1f6ee
Remah Amr <remah.alarabi@hamza.solutions>
2021-07-24 11:41:09 +0200

    sort posts

--- app/controllers/PostCtrl/fetchAll.js
+++ app/controllers/PostCtrl/fetchAll.js
@@ -4,31 +4,34 @@ const { APIResponse } = require("../../utils");
 const bcrypt = require("bcryptjs");
 const _ = require("lodash");
 
-module.exports = $baseCtrl(
-    async (req, res) => {
-        let typeDepth = req.params.postId ? 1 : req.params.commentId ? 2 : 0
-        console.log(typeDepth)
-        let posts = await models.post.fetchAll(
-            req.allowPagination,
-            {
-                ...(typeDepth === 1 && { directParent: req.params.postId }),
-                ...(typeDepth === 2 && { directParent: req.params.commentId }),
-                depth: typeDepth
-            },
-            {
-                ...req.queryOptions,
-                populate: [{ path: 'sharedPost', populate: { path: 'author', select: 'username photo' } }, { path: 'author', select: "username photo" }]
-            }
-        )
-
-        let all = posts.docs
-        for (let i = 0; i < all.length; i++) {
-            all[i] = all[i].toJSON({ authUser: req.me.id })
-        }
-        posts.docs = all
-
-
-
-        return APIResponse.Ok(res, posts)
+module.exports = $baseCtrl(async (req, res) => {
+  let typeDepth = req.params.postId ? 1 : req.params.commentId ? 2 : 0;
+  console.log(typeDepth);
+  let posts = await models.post.fetchAll(
+    req.allowPagination,
+    {
+      ...(typeDepth === 1 && { directParent: req.params.postId }),
+      ...(typeDepth === 2 && { directParent: req.params.commentId }),
+      depth: typeDepth,
+    },
+    {
+      ...req.queryOptions,
+      populate: [
+        {
+          path: "sharedPost",
+          populate: { path: "author", select: "username photo" },
+        },
+        { path: "author", select: "username photo" },
+      ],
+      sort: "-_id",
     }
-);
+  );
+
+  let all = posts.docs;
+  for (let i = 0; i < all.length; i++) {
+    all[i] = all[i].toJSON({ authUser: req.me.id });
+  }
+  posts.docs = all;
+
+  return APIResponse.Ok(res, posts);
+});
